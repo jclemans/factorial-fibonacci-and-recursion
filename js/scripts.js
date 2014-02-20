67e14fdf29c325abe655ff4b314dca8350acb8a0
epicodus <student@epicodus.com>
2014-02-20 14:14:58 -0800

    add non recursive fibonacci script

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,38 @@ var result = 1;
   };
   return result;
 };
+
+
+var fibonacci = function(number) {
+  var fibonacciArray = [];
+  var penultimate = 0;
+  var ultimate = 1;
+  var result;
+
+  if (number === 1) {
+    return 0;
+  } else if (number === 2) {
+    return 1;
+  } else {
+    for (var i = 2; i < number; i++) {
+      result = ultimate + penultimate;
+      fibonacciArray.push(result);
+      penultimate = ultimate;
+      ultimate = result; 
+    };
+  };
+  return result;
+};
+
+
+$(document).ready(function(){
+  $("#factorial").submit(function(event){
+    var number = $("#factorialInput").val();
+    var result = factorial(number);
+    
+    $("#factorialResult p").text(result);
+
+    event.preventDefault();    
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -8,6 +8,16 @@ describe('factorial', function() {
   it('takes a negative number and returns an error message.', function() {
     factorial(-1).should.equal("Please enter a positive integer.");
   });
-
 });
 
+describe('fibonacci', function() {
+  it('takes 1 and returns 0', function() {
+    fibonacci(1).should.equal(0);
+  });
+  it('takes 2 and return 1', function() {
+    fibonacci(2).should.equal(1);
+  });
+  it('takes an integer greater than 2 and returns the value of the Fibonacci sequence at that number', function() {
+    fibonacci(9).should.equal(21);
+  });
+});  
