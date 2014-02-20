2dde5fb7472d63735954a91ba9bfea412693abe5
epicodus <student@epicodus.com>
2014-02-20 11:51:03 -0800

    refactor js to utilize recursion

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,7 @@ var result = 1;
   if (number === 0) {
     return 1;
   } else if (number > 0) {
-    for (var n = number; n > 0; n--) {
-      result *= n;
-    }
+    result = number * (factorial(number - 1));
   } else {
     result = "Please enter a positive integer.";
   };
